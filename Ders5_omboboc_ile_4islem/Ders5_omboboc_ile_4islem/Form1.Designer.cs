a1e77661dedfbb0202874b8d9fb3b681e05b1098
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-15 10:16:26 +0300

    Menu strips added

--- Ders5_omboboc_ile_4islem/Ders5_omboboc_ile_4islem/Form1.Designer.cs
+++ Ders5_omboboc_ile_4islem/Ders5_omboboc_ile_4islem/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.üRÜNLERToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mUHASEBEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cARİToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.üRÜNGİRİŞFORMUToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.üRÜNLERToolStripMenuItem,
+            this.mUHASEBEToolStripMenuItem,
+            this.cARİToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1083, 45);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // üRÜNLERToolStripMenuItem
+            // 
+            this.üRÜNLERToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.üRÜNGİRİŞFORMUToolStripMenuItem});
+            this.üRÜNLERToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.üRÜNLERToolStripMenuItem.Name = "üRÜNLERToolStripMenuItem";
+            this.üRÜNLERToolStripMenuItem.Size = new System.Drawing.Size(146, 41);
+            this.üRÜNLERToolStripMenuItem.Text = "ÜRÜNLER";
+            // 
+            // mUHASEBEToolStripMenuItem
+            // 
+            this.mUHASEBEToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.mUHASEBEToolStripMenuItem.Name = "mUHASEBEToolStripMenuItem";
+            this.mUHASEBEToolStripMenuItem.Size = new System.Drawing.Size(165, 41);
+            this.mUHASEBEToolStripMenuItem.Text = "MUHASEBE";
+            // 
+            // cARİToolStripMenuItem
+            // 
+            this.cARİToolStripMenuItem.AutoToolTip = true;
+            this.cARİToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 20F);
+            this.cARİToolStripMenuItem.Name = "cARİToolStripMenuItem";
+            this.cARİToolStripMenuItem.Size = new System.Drawing.Size(86, 41);
+            this.cARİToolStripMenuItem.Text = "CARİ";
+            // 
+            // üRÜNGİRİŞFORMUToolStripMenuItem
+            // 
+            this.üRÜNGİRİŞFORMUToolStripMenuItem.Name = "üRÜNGİRİŞFORMUToolStripMenuItem";
+            this.üRÜNGİRİŞFORMUToolStripMenuItem.Size = new System.Drawing.Size(337, 42);
+            this.üRÜNGİRİŞFORMUToolStripMenuItem.Text = "ÜRÜN GİRİŞ FORMU";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1083, 589);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem üRÜNLERToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem üRÜNGİRİŞFORMUToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mUHASEBEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cARİToolStripMenuItem;
     }
 }
 
