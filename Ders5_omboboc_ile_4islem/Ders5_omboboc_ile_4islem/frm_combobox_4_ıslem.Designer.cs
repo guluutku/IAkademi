ce3a9b90a2eb9df7f323a40c2ed20c49844b0906
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-15 11:24:46 +0300

    Page load and button click added

--- Ders5_omboboc_ile_4islem/Ders5_omboboc_ile_4islem/frm_combobox_4_ıslem.Designer.cs
+++ Ders5_omboboc_ile_4islem/Ders5_omboboc_ile_4islem/frm_combobox_4_ıslem.Designer.cs
@@ -28,19 +28,163 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_firstNumber = new System.Windows.Forms.TextBox();
+            this.txt_secondNumber = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmb_process = new System.Windows.Forms.ComboBox();
+            this.btn_hesapla = new System.Windows.Forms.Button();
+            this.lbl_sonuc = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.label1.Location = new System.Drawing.Point(453, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 44);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "4 İşlem";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.label2.Location = new System.Drawing.Point(41, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(326, 44);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "1. SAYIYI GİRİNİZ";
+            // 
+            // txt_firstNumber
+            // 
+            this.txt_firstNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.txt_firstNumber.Location = new System.Drawing.Point(430, 130);
+            this.txt_firstNumber.Name = "txt_firstNumber";
+            this.txt_firstNumber.Size = new System.Drawing.Size(317, 50);
+            this.txt_firstNumber.TabIndex = 2;
+            // 
+            // txt_secondNumber
+            // 
+            this.txt_secondNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.txt_secondNumber.Location = new System.Drawing.Point(430, 214);
+            this.txt_secondNumber.Name = "txt_secondNumber";
+            this.txt_secondNumber.Size = new System.Drawing.Size(317, 50);
+            this.txt_secondNumber.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.label3.Location = new System.Drawing.Point(41, 214);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(326, 44);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "2. SAYIYI GİRİNİZ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.label4.Location = new System.Drawing.Point(123, 310);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(244, 44);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "İşlem Seçiniz";
+            // 
+            // cmb_process
+            // 
+            this.cmb_process.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.cmb_process.FormattingEnabled = true;
+            this.cmb_process.Location = new System.Drawing.Point(430, 310);
+            this.cmb_process.Name = "cmb_process";
+            this.cmb_process.Size = new System.Drawing.Size(317, 52);
+            this.cmb_process.TabIndex = 6;
+            // 
+            // btn_hesapla
+            // 
+            this.btn_hesapla.BackColor = System.Drawing.Color.Cornsilk;
+            this.btn_hesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.btn_hesapla.Location = new System.Drawing.Point(420, 453);
+            this.btn_hesapla.Name = "btn_hesapla";
+            this.btn_hesapla.Size = new System.Drawing.Size(238, 104);
+            this.btn_hesapla.TabIndex = 7;
+            this.btn_hesapla.Text = "HESAPLA";
+            this.btn_hesapla.UseVisualStyleBackColor = false;
+            this.btn_hesapla.Click += new System.EventHandler(this.btn_hesapla_Click);
+            // 
+            // lbl_sonuc
+            // 
+            this.lbl_sonuc.AutoSize = true;
+            this.lbl_sonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.lbl_sonuc.Location = new System.Drawing.Point(719, 394);
+            this.lbl_sonuc.Name = "lbl_sonuc";
+            this.lbl_sonuc.Size = new System.Drawing.Size(126, 44);
+            this.lbl_sonuc.TabIndex = 8;
+            this.lbl_sonuc.Text = "Sonuç";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label5.Location = new System.Drawing.Point(12, 512);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 31);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "TELEFON";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F);
+            this.label6.Location = new System.Drawing.Point(158, 512);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(193, 31);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "0506 502 5467";
             // 
             // frm_combobox_4_ıslem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1046, 579);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbl_sonuc);
+            this.Controls.Add(this.btn_hesapla);
+            this.Controls.Add(this.cmb_process);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_secondNumber);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_firstNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "frm_combobox_4_ıslem";
             this.Text = "frm_combobox_4_ıslem";
+            this.Load += new System.EventHandler(this.frm_combobox_4_ıslem_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_firstNumber;
+        private System.Windows.Forms.TextBox txt_secondNumber;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmb_process;
+        private System.Windows.Forms.Button btn_hesapla;
+        private System.Windows.Forms.Label lbl_sonuc;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
--- Ders5_omboboc_ile_4islem/Ders5_omboboc_ile_4islem/frm_combobox_4_ıslem.cs
+++ Ders5_omboboc_ile_4islem/Ders5_omboboc_ile_4islem/frm_combobox_4_ıslem.cs
@@ -16,5 +16,33 @@ namespace Ders5_omboboc_ile_4islem
         {
             InitializeComponent();
         }
+
+        private void frm_combobox_4_ıslem_Load(object sender, EventArgs e)
+        {
+            // Dizayndan yapılamıyacak kısımlar eklenir
+            // Dizaynda yapmak yerine kod kısmında gerçekleştir
+            // Dinamik olması için dizayn yerine kod kullan
+            cmb_process.Items.Add("TOPLAMA");
+            cmb_process.Items.Add("ÇIKARMA");
+            cmb_process.Items.Add("ÇARPMA");
+            cmb_process.Items.Add("BÖLME");
+        }
+
+        private void btn_hesapla_Click(object sender, EventArgs e)
+        {
+            int birinci_sayi = Convert.ToInt32(txt_firstNumber.Text);
+            int ikinci_sayi = Convert.ToInt32(txt_secondNumber.Text);
+            switch (cmb_process.SelectedItem)
+            {
+                case "TOPLAMA":
+                    break;
+                case "ÇIKARMA":
+                    break;
+                case "ÇARPMA":
+                    break;
+                case "BÖLME":
+                    break;
+            }
+        }
     }
 }
