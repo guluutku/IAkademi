49a84fcc1e70c748cd4f25bf1da19f0cd8854c2d
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-22 13:39:16 +0300

    Nüfus müdürlüğünden kişinin bilgileri kontrol ediliyor

---
+++ Ders7_Windows_Service_Nufus_Mudurlugu/Ders7_Windows_Service_Nufus_Mudurlugu/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Ders7_Windows_Service_Nufus_Mudurlugu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_kaydet = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_tc_kimlik = new System.Windows.Forms.TextBox();
+            this.txt_dogum_yili = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_soyad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_ad = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_adres = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_telefon_no = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_kaydet
+            // 
+            this.btn_kaydet.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.25F);
+            this.btn_kaydet.Location = new System.Drawing.Point(668, 419);
+            this.btn_kaydet.Name = "btn_kaydet";
+            this.btn_kaydet.Size = new System.Drawing.Size(290, 129);
+            this.btn_kaydet.TabIndex = 0;
+            this.btn_kaydet.Text = "KAYDET";
+            this.btn_kaydet.UseVisualStyleBackColor = true;
+            this.btn_kaydet.Click += new System.EventHandler(this.btn_kaydet_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(171, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TC KİMLİK";
+            // 
+            // txt_tc_kimlik
+            // 
+            this.txt_tc_kimlik.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.txt_tc_kimlik.Location = new System.Drawing.Point(390, 66);
+            this.txt_tc_kimlik.Name = "txt_tc_kimlik";
+            this.txt_tc_kimlik.Size = new System.Drawing.Size(211, 35);
+            this.txt_tc_kimlik.TabIndex = 2;
+            // 
+            // txt_dogum_yili
+            // 
+            this.txt_dogum_yili.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.txt_dogum_yili.Location = new System.Drawing.Point(390, 235);
+            this.txt_dogum_yili.Name = "txt_dogum_yili";
+            this.txt_dogum_yili.Size = new System.Drawing.Size(211, 35);
+            this.txt_dogum_yili.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label3.Location = new System.Drawing.Point(171, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(157, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "DOĞUM YILI";
+            // 
+            // txt_soyad
+            // 
+            this.txt_soyad.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.txt_soyad.Location = new System.Drawing.Point(390, 177);
+            this.txt_soyad.Name = "txt_soyad";
+            this.txt_soyad.Size = new System.Drawing.Size(211, 35);
+            this.txt_soyad.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label4.Location = new System.Drawing.Point(171, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 29);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "SOYAD";
+            // 
+            // txt_ad
+            // 
+            this.txt_ad.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.txt_ad.Location = new System.Drawing.Point(390, 119);
+            this.txt_ad.Name = "txt_ad";
+            this.txt_ad.Size = new System.Drawing.Size(211, 35);
+            this.txt_ad.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label5.Location = new System.Drawing.Point(171, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "AD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label2.Location = new System.Drawing.Point(171, 289);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 29);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "ADRES";
+            // 
+            // txt_adres
+            // 
+            this.txt_adres.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.txt_adres.Location = new System.Drawing.Point(390, 289);
+            this.txt_adres.Name = "txt_adres";
+            this.txt_adres.Size = new System.Drawing.Size(420, 35);
+            this.txt_adres.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label6.Location = new System.Drawing.Point(171, 363);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 29);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "TELEFON NO";
+            // 
+            // txt_telefon_no
+            // 
+            this.txt_telefon_no.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.txt_telefon_no.Location = new System.Drawing.Point(390, 363);
+            this.txt_telefon_no.Name = "txt_telefon_no";
+            this.txt_telefon_no.Size = new System.Drawing.Size(211, 35);
+            this.txt_telefon_no.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1036, 589);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txt_telefon_no);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_adres);
+            this.Controls.Add(this.txt_ad);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_soyad);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_dogum_yili);
+            this.Controls.Add(this.txt_tc_kimlik);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_kaydet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_kaydet;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_tc_kimlik;
+        private System.Windows.Forms.TextBox txt_dogum_yili;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_soyad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_ad;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_adres;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_telefon_no;
+    }
+}
+
---
+++ Ders7_Windows_Service_Nufus_Mudurlugu/Ders7_Windows_Service_Nufus_Mudurlugu/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ders7_Windows_Service_Nufus_Mudurlugu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_kaydet_Click(object sender, EventArgs e)
+        {
+            TcKontrolServiceReference.KPSPublicSoapClient t = new TcKontrolServiceReference.KPSPublicSoapClient();
+            bool result = t.TCKimlikNoDogrula(Convert.ToInt64(txt_tc_kimlik.Text), txt_ad.Text.ToUpper(), txt_soyad.Text.ToUpper(), Convert.ToInt32(txt_dogum_yili.Text));
+
+            if(result)
+            {
+                // müvekkillerin kaydını veritabanına yazacağım
+                MessageBox.Show("Kaydedildi");
+            }
+            else
+            {
+                MessageBox.Show("Bilgiler yanlış grildi");
+            }
+        }
+    }
+}
