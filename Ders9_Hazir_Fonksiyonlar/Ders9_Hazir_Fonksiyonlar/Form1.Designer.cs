ef0a549c8b755ed3335b4c2674fb1706b5377dc7
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-29 13:52:20 +0300

    Math. işlemleri gösterildi

--- Ders9_Hazir_Fonksiyonlar/Ders9_Hazir_Fonksiyonlar/Form1.Designer.cs
+++ Ders9_Hazir_Fonksiyonlar/Ders9_Hazir_Fonksiyonlar/Form1.Designer.cs
@@ -30,15 +30,17 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(225, 199);
+            this.button1.Location = new System.Drawing.Point(97, 208);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(216, 96);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Floor";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -49,12 +51,34 @@
             this.textBox1.Size = new System.Drawing.Size(199, 20);
             this.textBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(420, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(216, 96);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Ceiling";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(262, 327);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(216, 96);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "5\'e Göre";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -68,6 +92,8 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Ders9_Hazir_Fonksiyonlar/Ders9_Hazir_Fonksiyonlar/Form1.cs
+++ Ders9_Hazir_Fonksiyonlar/Ders9_Hazir_Fonksiyonlar/Form1.cs
@@ -19,12 +19,33 @@ namespace Ders9_Hazir_Fonksiyonlar
 
         private void button1_Click(object sender, EventArgs e)
         {
+            // küçüğe yuvarla
             textBox1.Text = Math.Floor(Convert.ToDecimal(textBox1.Text)).ToString();
+
+            // büyk olanı bul
+            // textBox1.Text = Math.Max(100, 200).ToString();
+
+            // küçük olanı bul
+            // textBox1.Text = Math.Min(100, 200).ToString();
+
+            // Math.Abs = mutlak değer = sayının pozitif hali
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // büyüğe yuvarlama
+            textBox1.Text = Math.Ceiling(Convert.ToDecimal(textBox1.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // 5'ten büyükse yukarı, 5'ten küçükse aşağı
+            textBox1.Text = Math.Round(Convert.ToDecimal(textBox1.Text)).ToString();
         }
     }
 }
