a46eda38cc954ad5583c9d7f497b3d1db6e65c93
Gün ULUUTKU <gunuluutku@gmail.com>
2023-07-30 10:27:38 +0300

    Button eklendi

--- Ders10_Hukuk_projesi/Ders10_Hukuk_projesi/Form1.cs
+++ Ders10_Hukuk_projesi/Ders10_Hukuk_projesi/Form1.cs
@@ -16,5 +16,10 @@ namespace Ders10_Hukuk_projesi
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
