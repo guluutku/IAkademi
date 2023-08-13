09f5988e0e126eb34168acf08d8b936019f62dfb
Gün ULUUTKU <gunuluutku@gmail.com>
2023-08-13 11:31:06 +0300

    Normaliza etme ve view yaratma

---
+++ IAkademi Query/Northwind/Normalisation.sql
@@ -0,0 +1,23 @@
+-- Normalisation (Normalizasyon)
+-- tabloları ve kolonları birleştirip, hangi tablolar birbiriyle ilişkili ona karar vereceğiz
+
+-- SELECT * FROM Products
+-- SELECT * FROM [NORTHWND].[dbo].[Categories]
+--------------------------
+
+CREATE VIEW vw_urunleri_listele -- Query'yi kaydeder
+as
+SELECT p.ProductID, p.ProductName, 
+c.CategoryID, c.CategoryName, 
+s.SupplierID, s.CompanyName
+FROM Products as p
+INNER JOIN Categories as c -- Normalisation yapımı
+ON p.CategoryID = c.CategoryID
+INNER JOIN Suppliers as s
+ON p.SupplierID = s.SupplierID
+
+
+----- alias
+-- SELECT * FROM Products as p
+
+SELECT * FROM vw_urunleri_listele -- view'i çağırma
