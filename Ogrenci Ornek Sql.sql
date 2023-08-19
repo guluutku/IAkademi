862cd8627e72cd4d88a25a4b99f7f79bd277f78f
Gün ULUUTKU <gunuluutku@gmail.com>
2023-08-19 10:11:04 +0300

    Ders notları

---
+++ Ogrenci Ornek Sql.sql
@@ -0,0 +1,7 @@
+-- Tbl_Ogrenci ( OgrenciID, Ad, Soyad, Cinsiyet, DTarih, Aktif)
+-- Tbl_Bolum (BolumID, BolumAdi)
+-- Tbl_Kitap ( KitapID, KitapAdi, IsbNo, YazarID, TurID, SayfaSayisi, Puan)
+-- Tbl_Yazar (YazarID, YazarAdi, YazarSoyadi)
+-- Tbl_Tur ( TurID, TurAdi)
+
+-- Tbl_Islem (IslemID, OgrenciID, KitapID, ATarih, VTarih) -- Ara Tablo
