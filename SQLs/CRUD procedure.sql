f0e7d97ea1819e4ea1ec57c34e2c73bb7be98977
Gün ULUUTKU <gunuluutku@gmail.com>
2023-08-20 10:05:09 +0300

    Insert ve Update procedure'lar eklendi

--- SQLs/CRUD procedure.sql
+++ SQLs/CRUD procedure.sql
@@ -1,13 +1,27 @@
--- insert
-create proc sp_insert_products
+-- Insert
+-- create procedure sp_insert_products
+--(
+--@ProductName nvarchar(40),
+--@UnitPrice money,
+--@UnitsInStock smallint,
+--@CategoryID int,
+--@SupplierID int
+--)
+--as
+--insert into [NORTHWND].[dbo].[Products] (ProductName, UnitPrice, UnitsInStock, CategoryID, SupplierID)
+--values (@ProductName, @UnitPrice, @UnitsInStock, @CategoryID, @SupplierID)
+
+-- Update
+create proc sp_update_products
 (
 @ProductName nvarchar(40),
 @UnitPrice money,
 @UnitsInStock smallint,
 @CategoryID int,
-@SupplierID int
+@SupplierID int,
+@ProductID int
 )
 as
-insert into [NORTHWND].[dbo].[Products] (ProductName, UnitPrice, UnitsInStock, CategoryID, SupplierID)
-values
-(@ProductName, @UnitPrice, @UnitsInStock, @CategoryID, @SupplierID)
+update [NORTHWND].[dbo].[Products] set ProductName = @ProductName, UnitPrice = @UnitPrice, UnitsInStock = @UnitsInStock, 
+CategoryID = @CategoryID, SupplierID = @SupplierID
+where ProductID = @ProductID
