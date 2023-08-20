d49fb950d845073cfe3f6879225740642490896b
Gün ULUUTKU <gunuluutku@gmail.com>
2023-08-20 10:10:11 +0300

    2 sayıyı toplama fonksiyonu

---
+++ Sqls/SQL Functions.sql
@@ -0,0 +1,12 @@
+﻿-- gunction (fonksiyon)
+-- 2 sayıyı toplayan func
+create function fn_toplama(@sayi1 int, @sayi2 int)
+returns int
+as
+begin
+	declare @toplam int
+	set @toplam = @sayi1 + @sayi2
+	return @toplam
+end
+---- test
+select dbo.fn_toplama(3,8)
