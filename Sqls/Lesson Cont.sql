2ea20ae296cb7aa4d0ddf202fdff10a3a31d13cb
Gün ULUUTKU <gunuluutku@gmail.com>
2023-08-26 13:13:32 +0300

    İsim değişti

---
+++ Sqls/Lesson Cont.sql
@@ -0,0 +1,8 @@
+﻿select E.EmployeeID, E.FirstName, O.OrderDate, O.ShippedDate, DATEDIFF(DAY, OrderDate, ShippedDate) as FARK from Orders as O
+inner join
+Employees as E
+on E.EmployeeID = O.EmployeeID
+-------
+select ISDATE('2009-02-29') -- tarih geçerliğini kontrol etme
+
+select COALESCE(Address, phone, 'iletişim bilgisi yok')
--- Sqls/Using Functions Together.sql
+++
@@ -1,4 +0,0 @@
-select E.EmployeeID, E.FirstName, O.OrderDate, O.ShippedDate, DATEDIFF(DAY, OrderDate, ShippedDate) as FARK from Orders as O
-inner join
-Employees as E
-on E.EmployeeID = O.EmployeeID
