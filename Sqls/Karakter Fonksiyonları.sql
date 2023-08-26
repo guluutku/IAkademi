f3fb1ea1f9f29226352cee7e5a804d65e111c191
Gün ULUUTKU <gunuluutku@gmail.com>
2023-08-26 10:15:43 +0300

    Sağdan seçim, büyük-küçük karakterlere dönüştürme

--- Sqls/Karakter Fonksiyonları.sql
+++ Sqls/Karakter Fonksiyonları.sql
@@ -4,3 +4,7 @@ select ASCII('A') -- Ascii tablosunda 65 nolu indexte olduğunu verir
 -- metin içinde aranmak istenen kısmı arar
 select CHARINDEX('ww','www.gunuluutku.com',0) -- bulursa index numarası, bulamazsa 0
 select LEFT('www.gunluutku.com',7) -- Solundan itibaren 7 karater getir, RIGHT'de kullanılabilir
+select RIGHT('www.gunluutku.com',7) 
+Select LEN('www.gunululu.com') -- uzunluk verir
+select LOWER('www.GUNulu.Com') -- hepsini küçük karaktere dönüştürür
+select UPPER('www.GUNulu.Com') -- hepsini büyük karaktere dönüştürür
