729f24392caf6254061a161bce0eafbefabc7e06
Gün ULUUTKU <gunuluutku@gmail.com>
2023-09-03 12:20:10 +0300

    Alter trigger added

--- SQLs/Create Insert Trigger.sql
+++ SQLs/Create Insert Trigger.sql
@@ -8,3 +8,14 @@ BEGIN
     WHERE ProductID = @ProductID
 END
 GO
+---------------
+ALTER TRIGGER [dbo].[trg_StockInsert] on [Order Details]
+FOR INSERT
+AS
+BEGIN
+    DECLARE  @Quantity int, @ProductID INT
+    SELECT @Quantity = Quantity, @ProductID = ProductID from inserted
+    UPDATE Products set UnitsInStock = UnitsInStock - @Quantity
+    WHERE ProductID = @ProductID
+END
+GO
