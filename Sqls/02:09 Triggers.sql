4b19d38ae11b152809b4c1cb715da7ca00f71ca9
Gün ULUUTKU <gunuluutku@gmail.com>
2023-09-08 17:56:43 +0300

    Created insertion selection trigger
    Added example for trigger

--- Sqls/02:09 Triggers.sql
+++ Sqls/02:09 Triggers.sql
@@ -10,8 +10,13 @@ BEGIN
 END
 
 -- Insertion example
+
 INSERT INTO Tbl_Ogrenci 
-VALUES('Tuğberk', 'Koçoğlu', 'E', '1996-12-14', 1, 1)
+VALUES('Duygu', 'Yiğit', 'K', '2002-12-14', 1, '10A', 1)
+
+INSERT INTO Tbl_Ogrenci 
+VALUES('Hamit', 'Canayakın', 'E', '2001-12-14', 1, '10A', 1)
+
 SELECT * FROM Tbl_Ogrenci
 UPDATE Tbl_Ogrenci SET Sinif = '10A'
 
@@ -25,3 +30,16 @@ DROP TRIGGER trg_listele ON Tbl_Ogrenci
 -- Disables and enables all of the triggers inside Tbl_Ogrenci table
 DISABLE TRIGGER ALL ON Tbl_Ogrenci
 ENABLE TRIGGER ALL ON Tbl_Ogrenci
+
+-- 10A sınıfına sadece kız öğrenciler kayıt yapabilsin
+-- Only female students can register 10A classroom
+CREATE TRIGGER trg_cinsiyet ON Tbl_Ogrenci
+FOR INSERT
+AS
+BEGIN
+    IF (exists(SELECT * FROM inserted WHERE Sinif = '10A' AND Cinsiyet = 'E'))
+        BEGIN
+            RAISERROR('10A Sınıfına Erkek Öğrenci Kaydı yapılamaz', 1, 1)
+            ROLLBACK TRANSACTION -- Reject action
+        END
+END
