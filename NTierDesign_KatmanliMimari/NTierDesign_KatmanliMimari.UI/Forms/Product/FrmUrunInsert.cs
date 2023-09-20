4c5af8ec799f87b13c16fad5d0e9a9ec2f7353f5
Gün ULUUTKU <gunuluutku@gmail.com>
2023-09-20 15:59:49 +0300

    References between layers added

---
+++ NTierDesign_KatmanliMimari/NTierDesign_KatmanliMimari.UI/Forms/Product/FrmUrunInsert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTierDesign_KatmanliMimari.UI.Forms.Product
+{
+    public partial class FrmUrunInsert : Form
+    {
+        public FrmUrunInsert()
+        {
+            InitializeComponent();
+        }
+    }
+}
