dacf3f288fd7c2b25f277aaeadecf46b462f0654
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-10-01 10:32:00 +0300

    Old HTML Project deleted new project added

---
+++ Ders39_Html_Dersleri/Ders39_Html_Dersleri/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ders39_Html_Dersleri.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
