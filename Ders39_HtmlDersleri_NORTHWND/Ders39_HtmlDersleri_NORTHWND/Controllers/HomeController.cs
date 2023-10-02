768dfa0050ae8ceaf29ced28b9b1a6b7b440a8e7
Gün ULUUTKU <gunuluutku@gmail.com>
2023-10-02 14:12:15 +0300

    Get list of data with EF and html, cshtml

--- Ders39_HtmlDersleri_NORTHWND/Ders39_HtmlDersleri_NORTHWND/Controllers/HomeController.cs
+++ Ders39_HtmlDersleri_NORTHWND/Ders39_HtmlDersleri_NORTHWND/Controllers/HomeController.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Ders39_HtmlDersleri_NORTHWND.Models;
 
 namespace Ders39_HtmlDersleri_NORTHWND.Controllers
 {
     public class HomeController : Controller
     {
+        
         // GET: Home
         public ActionResult Index()
         {
+            List<Categories> categoryList = Cls_Categroy.Select_Category_List();
+
+            return View(categoryList);
+        }
+
+        public ActionResult Kategorili_Urunler()
+        {
+            // select CategoryID From Categories
+
             return View();
         }
+
     }
 }
---
+++ Ders39_HtmlDersleri_NORTHWND/Ders39_HtmlDersleri_NORTHWND/Models/Cls_Categroy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ders39_HtmlDersleri_NORTHWND.Models
+{
+    public class Cls_Categroy
+    {
+
+        public static List<Categories> Select_Category_List()
+        {
+            using(NORTHWNDEntities nORTHWNDEntities = new NORTHWNDEntities())
+            {
+                List<Categories> categoriesList = nORTHWNDEntities.Categories.ToList();
+
+                return categoriesList;
+            }
+        }
+
+    }
+}
