1878f42b809559c61969991decedf4cad97534b8
Gün ULUUTKU <gunuluutku@gmail.com>
2023-10-02 13:09:31 +0300

    Entity Framework added to the project
    NORTHWND project added to the project

---
+++ Ders39_HtmlDersleri_NORTHWND/Ders39_HtmlDersleri_NORTHWND/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ders39_HtmlDersleri_NORTHWND.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
