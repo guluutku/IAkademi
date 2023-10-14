c3cc10b13f6881a8213c2f7b512e12f6f5d41e7d
Gün ULUUTKU <gunuluutku@gmail.com>
2023-10-14 10:14:02 +0300

    Project created

---
+++ Ders52_CSS_Dersleri/Ders52_CSS_Dersleri/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ders52_CSS_Dersleri.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
