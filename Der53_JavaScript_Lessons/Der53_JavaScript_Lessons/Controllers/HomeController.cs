509cded925667cf0358a2f8978359da1e26321ca
Gün ULUUTKU <gunuluutku@gmail.com>
2023-10-14 13:09:23 +0300

    Index2 created

--- Der53_JavaScript_Lessons/Der53_JavaScript_Lessons/Controllers/HomeController.cs
+++ Der53_JavaScript_Lessons/Der53_JavaScript_Lessons/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ namespace Der53_JavaScript_Lessons.Controllers
             return View();
         }
 
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
