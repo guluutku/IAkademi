40fdac46795c57008bf8f40ea7140f8ee8b49f44
Gün ULUUTKU <gunuluutku@gmail.com>
2023-10-14 13:07:31 +0300

    JavaScript project created

---
+++ Der53_JavaScript_Lessons/Der53_JavaScript_Lessons/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Der53_JavaScript_Lessons.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Der53_JavaScript_Lessons.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
