13bed8b1b50f2b4f8c6480378e7e588c890c1366
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-10-21 12:36:48 +0300

    Creating database table with codes

--- Ders56_ASP_NET_Core/Ders56_ASP_NET_Core/Models/Products.cs
+++ Ders56_ASP_NET_Core/Ders56_ASP_NET_Core/Models/Products.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,15 +7,14 @@ namespace Ders56_ASP_NET_Core.Models
 {
 	public class Products
 	{
-		public Products()
-		{
-		}
-
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Set Primary key
 		public int ProductID { get; set;}
 
-		[Required]
-        public string ProductName { get; set; }
+		[Required(ErrorMessage = "Ürün Adı Girmak Zorunlu")]
+		[MaxLength(40)]
+		[MinLength(3)]
+		[DisplayName("ÜRÜN ADI")] // Formda görünecek kısım
+        public string? ProductName { get; set; }
 
     }
 }
