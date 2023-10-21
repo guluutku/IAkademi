520ce50c27670933f00aa31d0db99f292e9ead59
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-10-21 10:56:26 +0300

    Unused old projects removed

--- Ders39_Html_Dersleri/Ders39_Html_Dersleri/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ders39_Html_Dersleri.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
