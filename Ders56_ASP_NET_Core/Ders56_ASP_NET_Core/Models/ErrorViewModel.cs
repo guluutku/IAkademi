260ec0a8f266cc95477a872f4f226f574884315f
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-10-21 10:55:56 +0300

    Net Core project created

---
+++ Ders56_ASP_NET_Core/Ders56_ASP_NET_Core/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Ders56_ASP_NET_Core.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
