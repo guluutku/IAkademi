a0d9c05f095f2bc082d340d7ad014e498e8fcbb9
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-10-28 13:50:35 +0300

    Admin Panel file added
    Empty for now

---
+++ iakademi38_proje/iakademi38_proje/Controllers/MainMenuController.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iakademi38_proje.Controllers
+{
+    public class MainMenuController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
+        public IActionResult DetailedSearch()
+        {
+            return View();
+        }
+    }
+}
