ecea4aba75e1da9828250288033a6d63f98fe652
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-10-28 10:28:16 +0300

    Project cleaned

--- iakademi38_proje/iakademi38_proje/Controllers/HomeController.cs
+++ iakademi38_proje/iakademi38_proje/Controllers/HomeController.cs
@@ -1,32 +1,16 @@
-﻿using System.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using iakademi38_proje.Models;
 
-namespace iakademi38_proje.Controllers;
-
-public class HomeController : Controller
+namespace iakademi38_proje.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Index()
+        {
+            return View();
+        }
     }
 }
-
