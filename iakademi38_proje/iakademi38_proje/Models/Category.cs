4a3a2dc19f00a26d38dee33977cc3e0206d534c0
Gün ULUUTKU <gunuluutku@gmail.com>
2023-11-04 10:37:53 +0300

    Lesson started

---
+++ iakademi38_proje/iakademi38_proje/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iakademi38_proje.Models
+{
+    public class Category
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CategoryID { get; set; }
+
+        public int ParentID { get; set; }
+
+        [StringLength(50, ErrorMessage = "En fazla 50 Karakter")]
+        [Required(ErrorMessage = "Kategori Adı Zorunlu Alan")]
+        [DisplayName("Kategori Adi")]
+        public string? CategoryName { get; set; }
+
+    }
+}
