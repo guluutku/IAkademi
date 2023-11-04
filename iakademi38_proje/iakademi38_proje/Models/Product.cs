488fc376437fe2582847b9b310ceffe097a7ac5e
Gün ULUUTKU <gunuluutku@gmail.com>
2023-11-04 11:10:58 +0300

    Taxes added

--- iakademi38_proje/iakademi38_proje/Models/Product.cs
+++ iakademi38_proje/iakademi38_proje/Models/Product.cs
@@ -13,7 +13,6 @@ namespace iakademi38_proje.Models
         [StringLength(100)]
         [Required(ErrorMessage = "Ürün adı girilmeli")]
         public string? ProductName { get; set; }
-
         public decimal? UnitPrice { get; set; }
 
         [DisplayName("Kategori")]
@@ -21,17 +20,29 @@ namespace iakademi38_proje.Models
 
         [DisplayName("Marka")]
         public int SupplierID { get; set; }
-
         public int Stock { get; set; }
-
         public int Discount { get; set; }
 
         [DisplayName("Statüs")]
         public int StatusID { get; set; }
-
         public DateTime AddDate { get; set; }
-
         public string? Keywords { get; set; }
+
+        // encapsulation
+        private int _Kdv { get; set; }
+        public int Kdv { 
+            get { return _Kdv; } 
+            set
+            {
+                _Kdv = Math.Abs(value);
+            }
+        }
+
+        public int HighLighted { get; set; } // öne çıkanlar
+        public int TopSeller { get; set; } // çok satanlar
+        public int Related { get; set; } // Buna Bakanlar
+        public string? Notes { get; set; }
+        public string? PhotoPath { get; set; }
 
     }
 }
