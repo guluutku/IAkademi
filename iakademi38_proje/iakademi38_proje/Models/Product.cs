3db46f70df9a7947da98b3b07f0eeafa71175df8
Gün ULUUTKU <gunuluutku@gmail.com>
2023-11-04 10:43:32 +0300

    Product class created for database creation

---
+++ iakademi38_proje/iakademi38_proje/Models/Product.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace iakademi38_proje.Models
+{
+    public class Product
+    {
+
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductID { get; set; }
+
+        [StringLength(100)]
+        [Required(ErrorMessage = "Ürün adı girilmeli")]
+        public string? ProductName { get; set; }
+
+        public decimal? UnitPrice { get; set; }
+
+        [DisplayName("Kategori")]
+        public int CategoryID { get; set; }
+
+        [DisplayName("Marka")]
+        public int SupplierID { get; set; }
+
+        public int Stock { get; set; }
+
+        public int Discount { get; set; }
+
+    }
+}
