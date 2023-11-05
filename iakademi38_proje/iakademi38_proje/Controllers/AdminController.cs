5302ea8275579bda3c150556a8acbf58d98cce99
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-11-05 10:40:36 +0300

    Admin login panel added

--- iakademi38_proje/iakademi38_proje/Controllers/AdminController.cs
+++ iakademi38_proje/iakademi38_proje/Controllers/AdminController.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using iakademi38_proje.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace iakademi38_proje.Controllers
 {
     public class AdminController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user)
         {
             return View();
         }
