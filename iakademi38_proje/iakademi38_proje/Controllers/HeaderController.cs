fe2bea5e5c1257529abbdb828311dbe9a5399aea
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-11-09 14:25:21 +0300

    Headers parted to specific folder

---
+++ iakademi38_proje/iakademi38_proje/Controllers/HeaderController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace iakademi38_proje.Controllers
+{
+    public class HeaderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            return View();
+        }
+        
+        public IActionResult DiscountedProducts()
+        {
+            return View();
+        }
+
+        public IActionResult HighlightedProducts()
+        {
+            return View();
+        }
+
+        public IActionResult MyOrders()
+        {
+            return View();
+        }
+
+        public IActionResult NewProducts()
+        {
+            return View();
+        }
+
+        public IActionResult SpecialProducts()
+        {
+            return View();
+        }
+
+        public IActionResult TopSelledProducts()
+        {
+            return View();
+        }
+
+    }
+}
