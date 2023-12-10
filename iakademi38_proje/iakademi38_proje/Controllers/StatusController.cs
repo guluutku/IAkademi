e0f45c17bd34e42e394074b0519cefdedb4c5a60
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-10 10:20:51 +0300

    Status pages created

--- iakademi38_proje/iakademi38_proje/Controllers/StatusController.cs
+++ iakademi38_proje/iakademi38_proje/Controllers/StatusController.cs
@@ -4,26 +4,103 @@ using System.Linq;
 using System.Threading.Tasks;
 using iakademi38_proje.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace iakademi38_proje.Controllers
 {
     public class StatusController : Controller
     {
-        /*
-        // list Status
-        public async Task<ActionResult> StatusIndex()
+        Cls_Status cls_Status = new Cls_Status();
+        iakademi38Context context = new iakademi38Context();
+
+        public async Task<IActionResult> StatusIndex()
         {
-            List<Status> status = await cls_Status.StatusSelect();
-
-            ViewBag.markaListesi = status;
-
-            return View("~/Views/Admin/Status/StatusIndex.cshtml", status);
+            List<Status> statuses = await cls_Status.StatusSelect();
+            return View("~/Views/Admin/Status/StatusIndex.cshtml", statuses);
         }
-        */
 
-        public IActionResult StatusIndex()
+        [HttpGet]
+        public IActionResult StatusCreate()
         {
-            return View("~/Views/Admin/Status/StatusIndex.cshtml");
+            return View("~/Views/Admin/Status/StatusCreate.cshtml");
+        }
+
+        [HttpPost]
+        public IActionResult StatusCreate(Status status)
+        {
+            bool answer = cls_Status.StatusInsert(status);
+            if (answer)
+            {
+                TempData["Message"] = "Eklendi";
+            }
+            else
+            {
+                TempData["Message"] = " HATA";
+            }
+            return RedirectToAction(nameof(StatusCreate));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> StatusEdit(int? id)
+        {
+            if (id == null || context.Statues == null)
+            {
+                return NotFound();
+            }
+
+            var status = await cls_Status.StatusDetails(id);
+
+            return View("~/Views/Admin/Status/StatusEdit.cshtml", status);
+        }
+
+        [HttpPost]
+        public IActionResult StatusEdit(Status status)
+        {
+            bool answer = Cls_Status.StatusUpdate(status);
+            if (answer)
+            {
+                TempData["Message"] = "Güncellendi";
+                return RedirectToAction("StatusIndex.cshtml");
+            }
+            else
+            {
+                TempData["Message"] = "HATA";
+                return RedirectToAction(nameof(StatusEdit));
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> StatusDelete(int? id)
+        {
+            if (id == null || context.Statues == null)
+            {
+                return NotFound();
+            }
+            var Status = await context.Statues.FirstOrDefaultAsync(s => s.StatusID == id);
+
+            if (Status == null)
+            {
+                return NotFound();
+            }
+            return View("~/Views/Admin/Status/StatusDelete.cshtml", Status);
+        }
+
+        // Url'de ActionName yazıyor
+        [HttpPost, ActionName("StatusDelete")]
+        public IActionResult StatusDeleteConfirmed(int id)
+        {
+            bool answer = Cls_Status.StatusDelete(id);
+            if (answer == true)
+            {
+                TempData["Message"] = "SİLİNDİ";
+                return RedirectToAction("StatusIndex");
+            }
+            else
+            {
+                TempData["Message"] = "HATA";
+                return RedirectToAction(nameof(StatusDelete));
+            }
+
         }
     }
 }
--- iakademi38_proje/iakademi38_proje/Models/Status.cs
+++ iakademi38_proje/iakademi38_proje/Models/Status.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace iakademi38_proje.Models
@@ -7,11 +8,15 @@ namespace iakademi38_proje.Models
     {
 
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [DisplayName("ID")]
         public int StatusID { get; set; }
 
         [StringLength(100)]
         [Required]
+        [DisplayName("Durum Adı")]
         public string? StatusName { get; set; }
+
+        [DisplayName("Aktif Mi?")]
         public bool Active { get; set; }
 
     }
