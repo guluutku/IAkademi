a01ee20c94b73420459f841695c5c47eea4d4ecb
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2023-12-24 13:38:26 +0300

    Update HeaderController.cs

--- iakademi38_proje/iakademi38_proje/Controllers/HeaderController.cs
+++ iakademi38_proje/iakademi38_proje/Controllers/HeaderController.cs
@@ -15,7 +15,7 @@ namespace iakademi38_proje.Controllers
 
         iakademi38Context context = new iakademi38Context();
 
-        int mainPageCount = 0;
+        int mainpageCount = 0;
 
         public IActionResult Index()
         {
@@ -24,8 +24,15 @@ namespace iakademi38_proje.Controllers
 
         public IActionResult NewProducts()
         {
-            mpm.NewProducts = cls_Product.ProductSelect("New", mainPageCount, "New", 0);
+            mpm.NewProducts = cls_Product.ProductSelect("New", mainpageCount, "New", 0);
             return View(mpm);
+        }
+
+        public PartialViewResult _PartialNewProducts(string pageno)
+        {
+            int pagenumber = Convert.ToInt32(pageno);
+            mpm.NewProducts = cls_Product.ProductSelect("New", mainpageCount, "New", pagenumber); //yeni
+            return PartialView(mpm);
         }
 
         public IActionResult SpecialProducts()
