ec49d914530e23db1724ecad832cdf329aaadd22
Gün ULUUTKU <48116748+guluutku@users.noreply.github.com>
2024-08-10 13:50:36 +0300

    Auto Update

--- iakademi38_proje/iakademi38_proje/obj/Debug/net7.0/iakademi38_proje.AssemblyInfo.cs
+++ iakademi38_proje/iakademi38_proje/obj/Debug/net7.0/iakademi38_proje.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("iakademi38_proje")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32f9bdb8e99e46c22a7aae7d9f4ca8da60d85b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("iakademi38_proje")]
 [assembly: System.Reflection.AssemblyTitleAttribute("iakademi38_proje")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
